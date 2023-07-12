782ed3919f3732f5eba739d52bf59fa3fb45ac4e
prajwalauti <128386417+prajwalauti@users.noreply.github.com>
2023-07-12 15:02:32 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,17 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+	         Scanner sc=new Scanner(System.in);
+	         System.out.println("Enter 1st Number = ");
+	         int x=sc.nextInt();
+	         System.out.println("Enter 2nd Number = ");
+	         int y=sc.nextInt();
+	         System.out.println("Addition of "+x+" and "+y+" is = "+(x+y));
+	       
+	}
+
+}
